87297c1e86e5398c358804f49be834b40a541175
Abdullah Arshad <abdullaharshadsialkot@gmail.com>
2025-08-04 21:04:16 +0500

    Footer changed

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ elif model_type == "Prophet":
 
 # -------------------------- Footer --------------------------
 st.markdown("---")
-st.markdown("<div style='text-align: center;'>Made by Khizer · Streamlit Forecast App</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center;'>Khizar Sabir (Dissertation Project)</div>", unsafe_allow_html=True)
